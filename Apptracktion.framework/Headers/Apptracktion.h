3447caf22a9617476d9a910609b46baa30881494
Innolabsdev <kevser.temiz@gmail.com>
2018-02-13 13:24:29 +0300

    ii

---
+++ Apptracktion.framework/Headers/Apptracktion.h
@@ -0,0 +1,19 @@
+//
+//  Apptracktion.h
+//  Apptracktion
+//
+//  Created by Xevser on 17/05/2017.
+//  Copyright © 2017 Medyasoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Apptracktion.
+FOUNDATION_EXPORT double ApptracktionVersionNumber;
+
+//! Project version string for Apptracktion.
+FOUNDATION_EXPORT const unsigned char ApptracktionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Apptracktion/PublicHeader.h>
+
+
